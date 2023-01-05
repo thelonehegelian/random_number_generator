7f9593b756ef785265cefc3e75ab8cc66e7ab22d
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-05 23:15:59 +0000

    Create random Point

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Generates random numbers using various methods
-use rand::distributions::Uniform;
+use rand::distributions::{Standard, Uniform};
 use rand::Rng;
 use rand_distr::{Distribution, Normal, NormalError};
 
@@ -40,6 +40,37 @@ fn main() -> Result<(), NormalError> {
     let normal = Normal::new(2.0, 5.0)?;
     let smpl = normal.sample(&mut rng);
     println!("normal: {}", smpl);
+    #[derive(Debug)]
+    // Generate random values of a custom type e.g. tuples
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    /*
+    The Distribution trait is a trait that defines how a type can be used to generate random values.
+    It has a single associated type, Sample, which represents the type of the values that can be generated.
+    The Standard type is a type that implements the Distribution trait for all types that implement the
+    Rand trait.
+    In this impl block, the Distribution trait is being implemented for the Standard type,
+    with the Point type as the Sample associated type. This means that the Standard type
+    can now be used to generate random values of type Point.
+    */
+
+    impl Distribution<Point> for Standard {
+        fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Point {
+            let x: i32 = rng.gen();
+            let y: i32 = rng.gen();
+            Point { x, y }
+        }
+    }
+
+    // create a random tuple
+    // The ::<> syntax is used to specify the type parameter for a function or method in Rust
+    let rand_tuple = rng.gen::<(i32, f32, f32)>();
+    let rand_point: Point = rng.gen();
+    println!("Random Tuple: {:?}", rand_tuple);
+    println!("Random Point: {:?}", rand_point);
 
     Ok(())
 }
