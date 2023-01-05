cc0b8cee3eb7dd1fe47d5528499971d493d6addf
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-05 07:58:57 +0000

    Generate and print random uints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let num1: u8 = rng.gen();
+    let num2: u16 = rng.gen();
+
+    println!("{}, {}", num1, num2);
+
+    let num3 = rand::random::<u32>();
+    let num4 = rand::random::<u64>();
+
+    println!("{}, {}", num3, num4);
 }
